15cc243cebeaf026586506c4e3f2e6e28fc37f58
François <frdebellescize@hotmail.fr>
2020-01-05 02:10:16 +0100

    adding the Person view

---
+++ lib/wttj_web/views/person_view.ex
@@ -0,0 +1,15 @@
+defmodule WttjWeb.PersonView do
+  use WttjWeb, :view
+
+  def render("index.json", %{persons: persons}) do
+    render_many(persons, WttjWeb.PersonView, "person.json")
+  end
+
+  def render("person.json", %{person: person}) do
+    %{
+      id: person.id,
+      name: person.name,
+      toMeet: person.toMeet
+    }
+  end
+end
